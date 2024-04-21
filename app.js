7dec6f540ed27be8151d11e1a69295fb8efd0095
yarzot <yarozot71@gmail.com>
2024-04-21 20:01:05 +0300

    best

---
+++ app.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const mainRouteController = require("./controllers/main")
+const defaultRouteController = require("./controllers/default")
+const gameRouteController = require("./controllers/game")
+const voteRouteController = require("./controllers/vote")
+
+const server = http.createServer((req, res) => {
+    const url = req.url;
+
+    switch(url) {
+        case "/":
+          mainRouteController(res, "/index.html", ".html");
+        break
+        case "/game":
+          gameRouteController(res);
+        break
+        case "/vote":
+          voteRouteController(req, res);
+        break
+        default:
+          defaultRouteController(res, url);
+        }
+      }); 
+
+server.listen(3000)
